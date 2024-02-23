8c0b4e77420d374551645f4c8b5b36956f65d1c3
leomillan22 <leonardo.amillan@outlook.com>
2024-02-23 14:11:06 -0300

    tela de login

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('princesadoscampos', 'root', '27012506',{
+const connection = new Sequelize('princesadoscampos', 'root', '123456',{
     host: 'localhost',
     dialect : 'mysql'
 })
